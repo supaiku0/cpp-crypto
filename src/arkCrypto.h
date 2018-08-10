1652d17389efd64f3b11e7b1dbf793d7e96dee7e
sleepdefic1t <sleepdefic1t@users.noreply.github.com>
2018-08-09 18:57:19 -0700

    update included headers

--- src/arkCrypto.h
+++ src/arkCrypto.h
@@ -1,18 +1,22 @@
-/***************************************
-* Ark-Cpp-Crypto - for the Ark Ecosystem (ark.io)
-* https://github.com/ArkEcosystem/Cpp-Crypto
-*
-* sleepdeficit(simon@ark.io)
-* MIT | Copyright S. Downey 2017-2018
-***************************************/
+/**************************************************
+ * This file is part of Ark Cpp Crypto.
+ *
+ * (c) Ark Ecosystem <info@ark.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ **************************************************/
 
 #ifndef ARKCRYPTO_H
 #define ARKCRYPTO_H
 
 #define HAS_CRYPTO
 
-#include "types/types.h"
-#include "models/models.h"
+#include "identities/privatekey.h"
+#include "identities/wif.h"
+#include "identities/publickey.h"
+#include "identities/address.h"
 
+using namespace Ark::Crypto::Identities;
 
 #endif
