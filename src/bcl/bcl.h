d2ff0a5a60423c8fc641a1c0ffd2fc229a680cb1
sleepdefic1t <sleepdefic1t@users.noreply.github.com>
2018-08-09 18:51:58 -0700

    move BCL folder to /src

---
+++ src/bcl/bcl.h
@@ -0,0 +1,17 @@
+
+
+#ifndef BCL_H
+#define BCL_H
+
+#include "Utils.hpp"
+#include "Base58Check.hpp"
+#include "CurvePoint.hpp"
+#include "Ecdsa.hpp"
+#include "FieldInt.hpp"
+#include "Ripemd160.hpp"
+#include "Sha256Hash.hpp"
+#include "Sha256.hpp"
+#include "Sha512.hpp"
+#include "Uint256.hpp"
+
+#endif
