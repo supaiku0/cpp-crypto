8efbefb96550e222f7017ee181341b9ea3013741
sleepdefic1t <sleepdefic1t@users.noreply.github.com>
2018-08-16 09:11:47 -0700

    changed map to array

--- src/configuration/fee.h
+++ src/configuration/fee.h
@@ -13,8 +13,6 @@
 #include "enums/types.h"
 #include "enums/fees.h"
 
-#include <unordered_map>
-
 using namespace Ark::Crypto::Enums;
 
 namespace Ark {
@@ -29,24 +27,25 @@ class Fee
 {
     private:
         /**************************************************
-         * @brief The default transaction fees.
+         * @brief The default transaction fees array.
          *
-         * @var const std::unordered_map<int, unsigned int> fees_
+         * note: implementation limits integer capacity ~ '4000000000'
          **************************************************/
-        std::unordered_map<int, unsigned int> fees_ = {
-            { Types::TRANSFER,                      Fees::TRANSFER },
-            { Types::SECOND_SIGNATURE_REGISTRATION, Fees::SECOND_SIGNATURE_REGISTRATION },
-            { Types::DELEGATE_REGISTRATION,         Fees::DELEGATE_REGISTRATION }, // note: implementation limits integer capacity ~ 4000000000
-            { Types::VOTE,                          Fees::VOTE },
-            { Types::MULTI_SIGNATURE_REGISTRATION,  Fees::MULTI_SIGNATURE_REGISTRATION },
-            { Types::IPFS,                          Fees::IPFS },
-            { Types::TIMELOCK_TRANSFER,             Fees::TIMELOCK_TRANSFER },
-            { Types::MULTI_PAYMENT,                 Fees::MULTI_PAYMENT },
-            { Types::DELEGATE_RESIGNATION,          Fees::DELEGATE_RESIGNATION }
+        unsigned int fees_[9] = {
+            Fees::TRANSFER,
+            Fees::SECOND_SIGNATURE_REGISTRATION,
+            Fees::DELEGATE_REGISTRATION,
+            Fees::VOTE,
+            Fees::MULTI_SIGNATURE_REGISTRATION,
+            Fees::IPFS,
+            Fees::TIMELOCK_TRANSFER,
+            Fees::MULTI_PAYMENT,
+            Fees::DELEGATE_RESIGNATION
         };
         /*************************************************/
 
     public:
+        Fee(){};
         unsigned int get(int type);
         void set(const int type, const unsigned int fee);
 };
