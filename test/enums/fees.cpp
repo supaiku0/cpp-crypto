0c9bfbdaae0045f5d7bff7c19cacb865339a3367
sleepdefic1t <sleepdefic1t@users.noreply.github.com>
2018-08-16 09:12:21 -0700

    update tests

--- test/enums/fees.cpp
+++ test/enums/fees.cpp
@@ -5,13 +5,31 @@
 TEST(enums, fees)
 {
     Ark::Crypto::Enums::Fees fees;
-    ASSERT_EQ(10000000u, fees.TRANSFER);
-    ASSERT_EQ(500000000u, fees.SECOND_SIGNATURE_REGISTRATION);
-    ASSERT_EQ(2500000000u, fees.DELEGATE_REGISTRATION);
-    ASSERT_EQ(100000000u, fees.VOTE);
-    ASSERT_EQ(500000000u, fees.MULTI_SIGNATURE_REGISTRATION);
-    ASSERT_EQ(0, fees.IPFS);
-    ASSERT_EQ(0, fees.TIMELOCK_TRANSFER);
-    ASSERT_EQ(0, fees.MULTI_PAYMENT);
-    ASSERT_EQ(0, fees.DELEGATE_RESIGNATION);
+    
+    auto feeZERO = fees.TRANSFER;
+    ASSERT_EQ(10000000, feeZERO);
+
+    auto feeONE = fees.SECOND_SIGNATURE_REGISTRATION;
+    ASSERT_EQ(500000000, feeONE);
+
+    auto feeTWO = fees.DELEGATE_REGISTRATION;
+    ASSERT_EQ(2500000000, feeTWO);
+
+    auto feeTHREE = fees.VOTE;
+    ASSERT_EQ(100000000, feeTHREE);
+
+    auto feeFOUR = fees.MULTI_SIGNATURE_REGISTRATION;
+    ASSERT_EQ(500000000, feeFOUR);
+
+    auto feeFIVE = fees.IPFS;
+    ASSERT_EQ(0, feeFIVE);
+
+    auto feeSIX = fees.TIMELOCK_TRANSFER;
+    ASSERT_EQ(0, feeSIX);
+
+    auto feeSEVEN = fees.MULTI_PAYMENT;
+    ASSERT_EQ(0, feeSEVEN);
+
+    auto feeEIGHT = fees.DELEGATE_RESIGNATION;
+    ASSERT_EQ(0, feeEIGHT);
 }
